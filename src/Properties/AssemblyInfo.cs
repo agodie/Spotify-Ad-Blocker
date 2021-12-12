11bfa39f7b688fde754ae6c61a78c8f0c0cd196a
agodie <agodie.developer@gmail.com>
2021-12-12 19:05:10 +0100

    Defined fork goals

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EZBlocker")]
-[assembly: AssemblyDescription("https://www.ericzhang.me")]
+[assembly: AssemblyTitle("YAEZBlocker")]
+[assembly: AssemblyDescription("YAEZBlocker: Yet Another EZBlocker Fork!  See https://github.com/agodie/Spotify-Ad-Blocker")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric Zhang")]
-[assembly: AssemblyProduct("EZBlocker")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2021 Eric Zhang")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YAEZBlocker")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.1.0")]
-[assembly: AssemblyFileVersion("1.8.1.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
